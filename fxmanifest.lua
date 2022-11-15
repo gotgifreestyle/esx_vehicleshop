b1ffd40e4979a1efd7189e6e6173a2d60af07c19
cyw_core <91406566+gotgifreestyle@users.noreply.github.com>
2022-11-15 22:41:40 +0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,38 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+description 'ESX Vehicle Shop / [edit by CYW_Core]'
+
+version '1.1.0'
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'@es_extended/locale.lua',
+	'locales/br.lua',
+	'locales/en.lua',
+	'locales/fi.lua',
+	'locales/fr.lua',
+	'locales/sv.lua',
+	'locales/pl.lua',
+	'config.lua',
+	'vehicle_data.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'locales/br.lua',
+	'locales/en.lua',
+	'locales/fi.lua',
+	'locales/fr.lua',
+	'locales/sv.lua',
+	'locales/pl.lua',
+	'config.lua',
+	'client/utils.lua',
+	'client/main.lua'
+}
+
+dependency 'es_extended'
+
+export 'GeneratePlate'
