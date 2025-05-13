2ae3c43287b7376221dc859037969c66377fec75
cyw_core <91406566+gotgifreestyle@users.noreply.github.com>
2025-05-13 23:40:47 +0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,7 +7,7 @@ description 'ESX Vehicle Shop / [edit by CYW_Core]'
 version '1.1.0'
 
 server_scripts {
-	'@mysql-async/lib/MySQL.lua',
+	'@oxmysql/lib/MySQL.lua',
 	'@es_extended/locale.lua',
 	'locales/br.lua',
 	'locales/en.lua',
